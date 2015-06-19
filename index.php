f58b7ae29aff7af3e1e680b737a45d8aba6f24f5
alchemyunited <mark.simchock@alchemyunited.com>
2015-06-19 10:27:40 -0400

    ux / ux poc

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-
-?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -10,9 +7,9 @@
 	
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 	
-	<title>ezBootstrapSandbox - A blank template for Bootstrap.</title>
-    <meta name="description" content="https://github.com/ezWebDevTools/ezBootstrapSandbox">
-    <meta name="author" content="">
+	<title>GC1</title>
+    <meta name="description" content="TODO">
+    <meta name="author" content="mfs">
     <link rel="icon" href="assets/img/dist/ico/favicon.ico">
 
     <!-- Bootstrap core CSS -->
@@ -34,21 +31,111 @@
   </head>
 
   <body>
+  <?php
 
-    <div class="container">
+    echo '<div class="container">';
 
-      <div class="intro">
-        <h1>ezBootstrapSandbox</h1>
+      echo '<div class="intro">';
+        echo '<h1>CG1</h1>';
+      echo '</div>';
+
+  echo '<div class="row">';
+?>
+  <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+      <div class="panel-wrap col-md-3">
+          <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingOne">
+              <h4 class="panel-title">
+                  <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                      Collapsible Group Item #1
+                  </a>
+              </h4>
+          </div>
+          </div>
+
+          <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingTwo">
+              <h4 class="panel-title">
+                  <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                      Collapsible Group Item #2
+                  </a>
+              </h4>
+          </div>
+          </div>
+
+          <div class="panel panel-default">
+          <div class="panel-heading" role="tab" id="headingThree">
+              <h4 class="panel-title">
+                  <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                      Collapsible Group Item #3
+                  </a>
+              </h4>
+          </div>
+          </div>
+
+
       </div>
 
-    </div><!-- /.container -->
+      <div class="col-md-9">
 
+          <div id="collapseZero" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingZero">
+              <div class="panel-body">
+                  0 - Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+              </div>
+          </div>
+
+              <div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+                  <div class="panel-body">
+                      1 - Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                  </div>
+              </div>
+
+          <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+              <div class="panel-body">
+                  2 - Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+              </div>
+          </div>
+
+
+          <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+              <div class="panel-body">
+                  3 - Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+              </div>
+          </div>
+
+      </div>
+  </div>
+  <?php
+  echo '</div>';
+
+  echo '</div>';
+
+    echo '</div><!-- /.container -->';
+?>
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+  <script>
+      $('#accordion').on('show.bs.collapse', function () {
+          $('.panel-collapse.collapse.in').removeClass('in');
+      });
+
+      $('#accordion').on('hidden.bs.collapse', function () {
+        //  alert($('.panel-collapse.collapse.in').length);
+          $('#collapseZero').addClass('in');
+          // do something…
+      })
+
+      $('.panel-wrap a').on('click', function(e){
+          $('.active').removeClass('active');
+          $(this).parents('.panel-heading').addClass('active');
+      });
+  </script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
